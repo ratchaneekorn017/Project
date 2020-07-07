9e2e0eb520c6a0d1b01e3b75a2cd27231e877019
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-07-08 03:04:01 +0700

    [Update] update connection integrated heroku

--- condb.php
+++ condb.php
@@ -1,5 +1,5 @@
 <?php
-	$conn= mysqli_connect("localhost","root","","projectweb") or die("Error : " . mysqli_error($conn));
+	$conn= mysqli_connect("us-cdbr-east-02.cleardb.com/","b9b339726be35f","47d8ee5","heroku_a3494caff9d39d0") or die("Error : " . mysqli_error($conn));
 	mysqli_query($conn, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE );
 date_default_timezone_set('Asia/Bangkok');
