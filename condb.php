1e75cb9e67c4ccb2fa79ae52108c385862f891c7
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-07-08 03:17:41 +0700

    test

--- condb.php
+++ condb.php
@@ -1,5 +1,5 @@
 <?php
-	$conn= mysqli_connect("us-cdbr-east-02.cleardb.com","b9b339726be35f","47d8ee5","heroku_a3494caff9d39d0") or die("Error : test" + mysqli_error($conn));
+	$conn= mysqli_connect("us-cdbr-east-02.cleardb.com","b9b339726be35f","47d8ee5","") or die("Error : test" . mysqli_error());
 	mysqli_query($conn, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE );
 date_default_timezone_set('Asia/Bangkok');
