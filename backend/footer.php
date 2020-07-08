d664f43fcfebf19a9450189c2a0454b2b1167f70
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-07-08 13:15:42 +0700

    [Update] footer

--- backend/footer.php
+++ backend/footer.php
@@ -1,42 +1,98 @@
-<!-- /.content-wrapper -->
-<footer class="main-footer">
-  <div class="pull-right hidden-xs">
-    <b></b> 
-  </div>
-  <strong> ข้าวตูแม่แป้ว &copy;  <a href=""></a></strong>
+<footer >
+     <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+  <footer>
+<div class="p-3 mb-2 bg-transparent-70 text-dark">
+    <div class="footer-area footer-padding" >
+        <div class="container">
+            <div class="row d-flex justify-content-between">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6">
+                   <div class="single-footer-caption mb-50">
+                     <div class="single-footer-caption mb-30">
+                          <!-- logo -->
+                         <!--div class="footer-logo mb-25">
+                             <a href="index.html"><img src="assets/img/logo/logok.png" alt=""></a>
+                         </div-->
+                         <div class="footer-tittle">
+                             <div class="footer-pera">
+                                 <h4> ข้าวตูแม่แป้ว</h4>
+                            </div>
+                         </div>
+                     
+                     </div>
+                   </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-4 col-sm-5">
+                    <div class="single-footer-caption mb-90">
+                        <div class="footer-tittle">
+                            <h4 class="text">เมนู</h4>
+                            <ul>
+                                <li><a class="text-dark" href="index.php">หน้าหลัก</a></li>
+                                <li><a class="text-dark" href="product.php">สินค้า</a></li>
+                                <li><a class="text-dark" href="contactus.php">ติดต่อเรา</a></li>
+                                <li><a class="text-dark" href="payment.php">แจ้งชำระเงิน</a></li>
+                                <li><a class="text-dark" href="seorder.php">ค้นหาออเดอร์</a></li>
+                                <li><a class="text-dark" href="form_login_m.php">เข้าสู่ระบบ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-7">
+                    <div class="single-footer-caption mb-50">
+                        <div class="footer-tittle">
+                            <h4 class="text-dark">ช่องทางติดต่อ</h4>
+                            <p class="text-dark">ข้าวตูแม่แป้ว 
+                                <br>ตลาดน้ำหลักห้า หมู่ 6 ต.ตาหลวง อ.ดำเนินสะดวก จ.ราชบุรี 70130</br>
+                                <br>โทร : 0982871123 
+                                <br>LINE : 0982871123
+                            
+                        </br>
+                    </br> 
+                            </p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
+                    <div class="single-footer-caption mb-50">
+                        <div class="footer-tittle">
+                            <h4 class="text-dark">เวลาทำการ</h4> 
+                                <p class="text-dark">จันทร์ - อาทิตย์
+                                <br>เวลา 09:00-18:00
+                                <br>ที่อยู่ ตลาดน้ำหลักห้า
+                                </br>
+                    </br> 
+</p> 
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer-bottom area -->
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="footer-border">
+                 <div class="row d-flex align-items-center">
+                     <div class="col-xl-20 ">
+                         <div class="footer-copy-right text-center">
+                             <!--<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+<!--Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                         </div>
+                     </div>
+                 </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Footer End-->
 </footer>
-</div>
-
-
-<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-
-<!-- page script -->
-
-
-<script>
-$(function () {
-$('#example1').DataTable()
-$('#example2').DataTable({
-'paging'      : true,
-'lengthChange': false,
-'searching'   : false,
-'ordering'    : false,
-'info'        : true,
-'autoWidth'   : false
-})
-})
-</script>
-<script>
-  $(function () {
-    // Replace the <textarea id="editor1"> with a CKEditor
-    // instance, using default configuration.
-    CKEDITOR.replace('editor1')
-    //bootstrap WYSIHTML5 - text editor
-    $('.textarea').wysihtml5()
-  })
-</script>
-</body>
-</html>
+  </div>
+  <!-- Copyright -->
+    
+      
+      
+ 
+</footer>
