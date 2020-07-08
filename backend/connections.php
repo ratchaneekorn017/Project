39c1e8a80a54a01888f0a5e322d674fa53f8a99d
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-07-08 13:01:41 +0700

    [Update] update db connection integrate heroku clearDB

--- backend/connections.php
+++ backend/connections.php
@@ -1,7 +1,8 @@
 <?php
-$con= mysqli_connect("localhost","root","","projectweb") or die("Error: " . mysqli_error($con));
-mysqli_query($con, "SET NAMES 'utf8' ");
+	$con= mysqli_connect("us-cdbr-east-02.cleardb.com","b9b339726be35f","47dd8ee5","heroku_a3494caff9d39d0") or die("Error :" . mysqli_error($conn));
+	mysqli_query($conn, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE );
 date_default_timezone_set('Asia/Bangkok');
- 
+// $conn = pg_connect("host=ec2-52-204-232-46.compute-1.amazonaws.com dbname=dfeupmn2kslm1n user=qhthkglklzzsfc password=5355edb6ce8ccb53e95d0cd067f27d0705edd9b0f5af6ae244b15713fa4c65e3")
+//     or die('Could not connect: ' . pg_last_error());
 ?>
